8f70bbe6e367b5b4c10a1612c0945e45a0aeea1b
sebastianduarte13 <jsdr137@gmail.com>
2024-05-02 12:57:02 -0500

    firt commit

---
+++ heroes.js
@@ -0,0 +1,12 @@
+let marvel ={
+    name:'Tony stark',
+    codeName : 'Ironman',
+    isAlive : false,
+    age : 40,
+    coord : {
+        lat : 35.056,
+        lng : -7.0789
+    },
+    suit : ['Mark I', 'Marl V', 'Hulkbuster']
+}
+console.log(marvel)
---
+++ heroes2.js
@@ -0,0 +1,21 @@
+let marvel ={
+    name:'Tony stark',
+    codeName : 'Ironman',
+    isAlive : false,
+    age : 40,
+    coord : {
+        lat : 35.056,
+        lng : -7.0789
+    },
+    suit : ['Mark I', 'Marl V', 'Hulkbuster'],
+    address: {
+        zip:'680004',
+        location : 'Malabu, California'
+    }
+}
+//console.log(marvel.name)
+//console.log(marvel.age)
+/*console.log(marvel.coord);
+console.log(`latitud ${marvel.coord.lat}`)
+console.log(`latitud ${marvel.coord.lng}`)*/
+console.log(`Total de trajes `)
